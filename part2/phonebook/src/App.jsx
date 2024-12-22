14faa9304c8a48c83f1034b64f4b749483828aa9
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2024-12-21 21:27:11 -0800

    Exercise 2.8 completed, added a number element in the person object.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 const Person = ({person}) => {
   console.log(`This person's name is ${person.name}`)
   return (
-    <li > {person.name} </li>
+    <li > {person.name} : {person.number} </li>
   )
 }
 
@@ -16,14 +16,15 @@ const Title = ({text}) => {
 }
 
 // Form to add a new person
-const Form = ({addPerson, newPerson, onChange}) => {
-
+const Form = ({addPerson, newName, newNumber, onNameChange, onNumChange}) => {
 
   return (
     <form onSubmit={addPerson}>
       <div>
-        Name: <input value={newPerson} onChange={onChange} />
+        Name: <input value={newName} onChange={onNameChange} />
+        Number: <input value={newNumber} onChange={onNumChange} />
       </div>
+
       <div>
         <button type="submit">add</button>
       </div>
@@ -48,40 +49,49 @@ const Persons = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-12-34567' }
   ]) 
-  const [newPerson, setPerson] = useState('')
+  const [newName, setName] = useState('')
+  const [newNumber, setNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked')
 
     // If name already exists in the list, prevented from adding + a notice
-    if (persons.some(person => person.name === newPerson)) {
-      alert(`${newPerson} is already added to phonebook`);
+    if (persons.some(person => {
+      return person.name === newName && person.number === newNumber})) {
+      alert(`${newName} is already added to phonebook`);
       return; // Exit the function
     }
 
     // creates a new object with provided input
     const personObject = {
-      name: newPerson
+      name: newName,
+      number: newNumber
     }
 
     // adds the new person object to the persons list
     setPersons(persons.concat(personObject))
-    setPerson('')
+    setName('')
+    setNumber('')
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
-    setPerson(event.target.value)
+    setName(event.target.value)
+  }
+
+  const handleNumChange = (event) => {
+    console.log(event.target.value)
+    setNumber(event.target.value)
   }
 
   return (
     <div>
 
       <Title text={"Phonebook"} />
-      <Form addPerson={addPerson} newPerson={newPerson} onChange={handlePersonChange}/>
+      <Form addPerson={addPerson} newName={newName} newNumber={newNumber} onNameChange={handleNameChange} onNumChange={handleNumChange}/>
       <br />
 
       <Title text={"Numbers"} />
