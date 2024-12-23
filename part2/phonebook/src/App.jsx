5abc45a4b08849d5cd488096777196042cb1199f
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2024-12-22 21:56:37 -0800

    Created an update function to update the number of a contact if same name, but different number. Server updates the person correctly, but I'm not rendering correctly on frontend. Need to look into + the missing key error.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import personService from './services/persons'
 
 // Passed the whole object for person, displays individual names
-const Person = ({person}) => {
+const Person = ({person, onClick}) => {
   console.log(`This person's name is ${person.name}`)
   return (
-    <li > {person.name} : {person.number} </li>
+    <li key={person.id}> {person.name} : {person.number} <button onClick={() => onClick(person.id)}>Delete</button> </li>
   )
 }
 
@@ -37,15 +36,15 @@ const Form = ({addPerson, newName, newNumber, onNameChange, onNumChange}) => {
 }
 
 // Component returns a list of all the people stored in the persons object
-const Persons = ({persons}) => {
+const Persons = ({persons, onClick}) => {
   return (
     <div>
       <Title text={"Numbers"} />
-      <ol>
+      <ol key={persons.id} >
         {persons.map(person => {
           return(
-            <div key={person.name}>
-              <Person person={person} />
+            <div key={person.id} >
+              <Person person={person} onClick={onClick} />
             </div> 
           )
         })}
@@ -82,33 +81,84 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked')
-
-    // If name already exists in the list, prevented from adding + a notice
-    if (persons.some(person => {
-      return person.name === newName && person.number === newNumber})) {
-      alert(`${newName} is already added to phonebook`);
-      return; // Exit the function
-    }
-
-    // keeps the ids in numerical order
+  // keeps the ids in numerical order
     const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0;
 
+    // Saves id as a string instead of int
+    const newId = (maxId + 1).toString()
     // creates a new object with provided input
+    
+
+    const person = persons.filter(person => person.name === newName);
+
+
+    const personBool = persons.find((person) => person.name === newName && person.number === newNumber);
     const personObject = {
       name: newName,
       number: newNumber,
-      id: maxId + 1
+      id: newId
     }
+    const sameId = person[0].id
+    const updatedPersonObject = {
+      name: newName,
+      number: newNumber,
+      id: sameId 
+    }
+    console.log("This is the person containing result", person)
+    console.log("This is the person containing result, ID ", sameId)
+    console.log("This is the new number ", personObject.number);
+    console.log("This is the person object with new number", personObject)
+    
+    // If name already exists in the list, prevented from adding + a notice
+    if (personBool) {
+      alert(`${newName} is already added to phonebook`);
+      console.log("Name already in phonebook, return")
+      return; // Exit the function
+    } 
 
-    personService
+    const result =  persons.find(person => person.name === newName && person.number !== newNumber) //if name is found in the db
+    ? update(updatedPersonObject) //user is given choice to update the phone number
+    : personService //if name is not found, the new person is added to the db
       .create(personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        setName('')
-        setNumber('')
-      })
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson));
+        setName('');
+        setNumber('');
+      });
+
+      return result;
   }
 
+  const update = (person) => {
+    console.log("First message inside of update, acknowledge same name and different number")
+    const confirmUpdate = window.confirm(`${newName} is already added to the phonebook. Replace the old number with the new one?` )
+    console.log(`This is id ${person.id}`)
+  
+
+    if (confirmUpdate) {
+      console.log("Yes was clicked");
+      console.log(`This is the person object ${person.name}`)
+
+      const changedPerson = { ...person}
+
+      changedPerson.number = person.number
+      console.log(`This is the changed person ${changedPerson.number}`)
+
+      console.log(`This is the changed person id ${changedPerson.id}`)
+      
+      return (
+        personService
+        .update(person.id, changedPerson)
+        .then(updatedPerson => {
+          setPersons(persons.map(person => person.id !== changedPerson.id ? person : updatedPerson))
+          setName('')
+          setNumber('')
+        })
+      )
+    }
+  }
+
+  
   const handleNameChange = (event) => {
     setName(event.target.value)
   }
@@ -120,7 +170,22 @@ const App = () => {
   const handleSearchChange = (event) => {
     setSearch(event.target.value)
   }
+  const deletePerson = (id) => {
+    // Saves the person object with the id being deleted
+    const personToDelete = persons.find(person => person.id === id);
 
+    if (personToDelete && window.confirm(`Delete ${personToDelete.name}?`)) {
+    // calls axios service to delete the person with the matching id
+    personService
+        .deletes(personToDelete.id)
+        .then(() => {
+          // returns and stores list in persons variable and excludes the deleted person
+          setPersons(persons.filter(person => person.id !== personToDelete.id));
+        })
+        .catch(error => {
+          alert(`The person '${personToDelete.name}' was already deleted from the server`);
+        })
+  }}
   // Saves a list of people that match the name in the search. If no search input, 
   // then the whole list of people is returned
   const filteredPersons = search
@@ -134,7 +199,7 @@ const App = () => {
       <Form addPerson={addPerson} newName={newName} newNumber={newNumber} onNameChange={handleNameChange} onNumChange={handleNumChange}/>
       <br />
       <Filter search={search} onChange={handleSearchChange} />
-      <Persons persons={filteredPersons} />  
+      <Persons persons={filteredPersons} onClick={deletePerson} />  
 
     </div>
   )
